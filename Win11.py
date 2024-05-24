2443a093e1c04dacbdcbdb7b219545430ed0c35f
Inocente-cmd <154298081+Inocente-cmd@users.noreply.github.com>
2024-05-24 18:21:18 -0400

    Update Win11.py

--- Win11.py
+++ Win11.py
@@ -19,3 +19,20 @@ while True:
 
     # Esperar el intervalo de tiempo antes de la próxima actividad
     time.sleep(intervalo_actividad)
+
+
+"""
+0x08: Tecla Retroceso (Backspace)
+0x09: Tecla Tabulación (Tab)
+0x0D: Tecla Enter
+0x1B: Tecla Escape
+0x20: Barra Espaciadora
+0x25: Tecla Flecha Izquierda
+0x26: Tecla Flecha Arriba
+0x27: Tecla Flecha Derecha
+0x28: Tecla Flecha Abajo
+0x30 a 0x39: Teclas de 0 a 9
+0x41 a 0x5A: Teclas de A a Z
+0x60 a 0x69: Teclas Numéricas (0 a 9) en el teclado numérico
+0x70 a 0x87: Teclas de Función (F1 a F24)
+"""
